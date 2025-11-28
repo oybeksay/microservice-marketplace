26325b90ef07ad84c072ef308ca60434e082e05a
Oybek <saydaliyev0710@gmail.com>
2025-11-28 17:22:25 +0500

    inserted basic CRUD

--- book-service/src/main/java/microservice/bookservice/config/SecurityConfig.java
+++ book-service/src/main/java/microservice/bookservice/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package microservice.bookservice.config;
 
-import microservice.bookservice.filter.AuthFilter;
+import microservice.bookservice.security.AuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
